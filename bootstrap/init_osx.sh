e1f05eb6a7f6890c7ffc19b93ece3cce70533ec0
Zaki Juhari <developer@zakijuhari.com>
2013-09-11 22:23:06 +0800

    Added sudo keep-alive magic.
    https://gist.github.com/cowboy/3118588

--- bootstrap/init_osx.sh
+++ bootstrap/init_osx.sh
@@ -16,6 +16,9 @@ fi
 # Ask for password only at the beginning
 sudo -v 
 
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # ------
 # Setup Hostname and etc
 # ------
